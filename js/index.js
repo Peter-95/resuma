347ec19de0610bb335985ff5efa542af8a6c8c92
Peter-95 <57274805+Peter-95@users.noreply.github.com>
2020-01-18 20:23:22 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 50 ){
+    $('.navbar').css('background' ,'#2B2B2B'); 
+    $('.navbar-toggler').css('hover', '#FFF');
+    } else{
+        $('.navbar').css ('background' ,'transparent');
+    }
+});
