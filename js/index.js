f65b80afa37cadf2aff83c585b8ef97def77af05
Peter-95 <57274805+Peter-95@users.noreply.github.com>
2020-01-25 22:45:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 50 ){
-    $('.navbar').css('background' ,'#2B2B2B'); 
-    $('.navbar-toggler').css('hover', '#FFF');
-    } else{
-        $('.navbar').css ('background' ,'transparent');
-    }
-});
-
-
 $(document).ready(function(){
-    $('#btn-feadback').click(function(){
-       $('#feedback-form').fadeToggle('slow');
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= 50 ){
+            $('.navbar').css('background' ,'#2B2B2B'); 
+            $('.navbar-toggler').css('hover', '#FFF');
+            } else{
+                $('.navbar').css ('background' ,'transparent');
+            }
         });
+        
+        $('#btn-feadback').click(function(){
+            $('#feedback-form').fadeToggle('slow');
+             });
+	
+        $(window).scroll(function(){
+            if ($(this).scrollTop() >=200) 
+            {
+                $('.scroll-animet').slideDown(1000);
+            } 	else 
+            {
+                $('.scroll-animet').slideUp(1000);
+            }
+ 
+        });
+        
+ 
 });
